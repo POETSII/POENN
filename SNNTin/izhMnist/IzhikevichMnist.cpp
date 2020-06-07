7a2ecc9325fb5676139f0d94ecb1b831e21a93f3
mahyar64 <mahyar.shahsavari@gmail.com>
2020-06-07 21:00:16 +0200

    add MNIST as input of izhikevich neuron in RBM multilayer network

---
+++ SNNTin/izhMnist/IzhikevichMnist.cpp
@@ -0,0 +1,200 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// (Based on code by David Thomas)
+
+#include <EdgeList.h>
+#include <assert.h>
+#include <sys/time.h>
+#include "RNG.h"
+#include "mnist.h"
+
+#define NUM_STEPS 100
+
+// Neuron 
+struct Neuron {
+  // Random-number-generator state
+  uint32_t rng, spikeTime [100]={0};
+  uint32_t inputData;  
+ // Neuron state
+  float u, v, I, spikeCount ;
+  // Neuron properties
+  float a, b, c, d, Ir;
+};
+
+int main(int argc, char**argv)
+{
+  if (argc != 2) {
+    printf("Specify edges file\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Read network
+  EdgeList net;
+  net.read(argv[1]);
+
+  // Ratio of excitatory to inhibitory neurons
+  double excitatory = 0.8;
+
+  // Mark each neuron as excitatory (or inhibiatory)
+  srand(1);
+  bool* excite = new bool [net.numNodes];
+  for (int i = 0; i < net.numNodes; i++) {
+    excite[i] = urand() < excitatory;
+  }
+
+  // Edge weights
+  float** weight = new float* [net.numNodes];
+  for (int i = 0; i < net.numNodes; i++) {
+    uint32_t numEdges = net.neighbours[i][0];
+    weight[i] = new float [numEdges];
+    for (int j = 0; j < numEdges; j++) {
+      weight[i][j] = excite[i] ? 0.5 * urand() : -urand();
+    }
+  }
+  //Add mnist data-set 
+float IMnist [784],temp;
+int k=0;
+
+//for (int k=0; k<2;k++){ //k should be 59900 instead of 60000
+int ** mnarray=readMnist();
+       	for (int i=0;i<28;i++)
+	   for (int j=0;j<28;j++)
+		{
+		//printf("%d ", mnarray[i][j]);
+		temp =mnarray[i][j];
+		IMnist [k]=temp/255;
+		//printf("%f ", IMnist [k]);
+		k++;
+		} 
+
+  // State for each neuron
+  srand(2);
+  Neuron* neuron = new Neuron [net.numNodes];
+  
+  for (int i = 0; i < net.numNodes; i++) {
+    Neuron* n = &neuron[i];
+    n->rng = (int32_t) (urand()*((double) (1<<31)));
+    //printf("rand=%lf\n", urand());
+    //printf("I=%d\n", n->rng);
+    if (excite[i]) {
+      float re = (float) urand();
+      n->a = 0.02;
+      n->b = 0.2;
+      n->c = -65+15*re*re;
+      n->d = 8-6*re*re;
+      n->Ir = 20;
+    }
+    else {
+      float ri = (float) urand();
+      n->a = 0.02+0.08*ri;
+      n->b = 0.25-0.05*ri;
+      n->c = -65;
+      n->d = 2;
+      n->Ir = 2;
+    }
+  }
+
+  // Spike array
+  bool* spike = new bool [net.numNodes];
+
+  // Initialisation
+  //for (int i = 0; i < net.numNodes; i++) {
+  for (int i = 0; i < 784; i++) {
+   Neuron* n = &neuron[i];
+    n->v = -65.0;
+    n->u = n->b * n->v;
+   // n->I = n->Ir * grng(n->rng);
+   n->I = n->Ir * IMnist [i];
+    printf("I=%f\n", n->I);
+  }
+
+
+  // Timer
+  printf("Started\n");
+  struct timeval start, finish, diff;
+  gettimeofday(&start, NULL);
+   // Simulation
+  float vTemp;
+  int64_t totalSpikes = 0;
+  for (int t = 0; t <= NUM_STEPS; t++) {
+    // Update state
+    for (int i = 0; i < net.numNodes; i++) {
+      spike[i] = false;
+      Neuron* n = &neuron[i];
+      float &v = n->v;
+      float &u = n->u;
+      float &I = n->I;
+     // printf("inrput: %f\n", I);
+    //  v = v+0.5*(0.04*v*v+5*v+140-u+I); // Step 0.5 ms
+      v = v+0.5*(0.04*v*v+5*v+140-u+I); // for numerical
+  // activation potential plot data for single neuron
+	  if (i==20)
+		  {
+		    if (v <= -65)
+     			vTemp=-65;
+		    else if (v >=0)
+			vTemp=0;
+		    else 
+			vTemp=v;
+	//	    printf("%d  %f\n",t,vTemp);
+		  }
+
+
+      u = u + n->a*(n->b*v-u);          // stability
+      if (v >0) {
+        n->v = n->c;
+        n->u += n->d;
+        spike[i] = true;
+      }
+
+     //if (net.numNodes <= 784)
+    // n->I = n->Ir * Mnist(i)
+
+      n->I = n->Ir * grng(n->rng);
+    }
+    // Update I-values
+    uint32_t spikes = 0;
+    for (int i = 0; i < net.numNodes; i++) {
+      Neuron* n = &neuron[i];
+      if (spike[i]) {
+        spikes++;
+        n->spikeCount++;
+        n->spikeTime [t]=t ;
+        uint32_t numEdges = net.neighbours[i][0];
+        uint32_t* dst = &net.neighbours[i][1];
+        for (int j = 0; j < numEdges; j++) {
+          neuron[dst[j]].I += weight[i][j];
+        }
+      }
+    }
+  //  printf("%d: %d\n", t, spikes);
+  // printf("%d: %d\n", t, spikeCount++);
+
+    totalSpikes += spikes;
+  }
+ gettimeofday(&finish, NULL);
+ /* //add for raster file
+  FILE *output_file;
+  output_file=fopen("nSpikes.csv", "w");
+
+
+    for (int i;i<net.numNodes;i++)
+ 	 {
+		Neuron* n = &neuron[i];		 
+		fprintf(output_file,"%d ",i);
+	 for (int j=0;j<100;j++)
+	 //	printf("%d",spike[j]);
+	     fprintf(output_file,"%d ",neuron[i].spikeTime [j]);  
+	    fprintf(output_file,"\n");
+
+	 }
+  	fclose(output_file);
+	*/
+    printf("Total spikes: %ld\n", totalSpikes);
+
+  // Display time
+  timersub(&finish, &start, &diff);
+  double duration = (double) diff.tv_sec + (double) diff.tv_usec / 1000000.0;
+  printf("Time = %lf\n", duration);
+
+  return 0;
+}
